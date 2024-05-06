3b922dcb1c2162b3a1428d494fa8a8f0ca4f6500
keniaestrada <kenialbsg@gmail.com>
2024-05-06 17:19:08 -0600

    Primer cambio

---
+++ Pruebas.js
@@ -0,0 +1,21 @@
+/*Ésta entrega tiene como objetivo reafirmar y practicar los temas vistos en la sesión de Pruebas Unitarias con JEST para JavaScript.
+
+Después de la sesión síncrona con el equipo de instrucción realiza ésta práctica alojada en Github para después entregar por medio de un link tu fork del repositorio original. Asegúrate de que sea un repositorio público para poder compartir y permitir su revisión.
+
+Instrucciones
+
+1. Realiza un fork del repositorio de práctica para las pruebas unitarias.Links to an external site.
+
+2. Clona localmente tu fork y accede a él.
+
+3. Lee cuidadosamente las instrucciones y escribe diferentes pruebas unitarias para los componentes listados en el README.md
+
+Notas
+
+Todos los archivos necesarios para realizar el ejercicio se encuentran en el repositorio. Para algunos componentes será necesario escribir el algoritmo + escribir sus pruebas.
+
+No se aceptarán PR hacia el repo original.
+
+Entrega
+
+Entrega solamente el link hacia tu fork. Éste debería tener ya todos ejercicios realizados.*/
